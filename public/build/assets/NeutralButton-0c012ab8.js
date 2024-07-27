dc2c0df74e9d0152fe95be1af4d72b334f283dd2
Harry <hazzy995@gmail.com>
2024-07-27 17:39:41 +0100

    missing assets

---
+++ public/build/assets/NeutralButton-0c012ab8.js
@@ -0,0 +1,1 @@
+import{o as s,f as o,s as n}from"./app-428aceeb.js";const r=["type"],c={__name:"NeutralButton",props:["submit"],setup(t){return(e,a)=>(s(),o("button",{class:"btn btn-neutral",type:t.submit?"submit":"button"},[n(e.$slots,"default")],8,r))}};export{c as _};
