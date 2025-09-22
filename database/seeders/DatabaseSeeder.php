4b4ec88be20d8cc58c51942ca3bcfaa92fc18967
Harry <hazzy995@gmail.com>
2025-09-22 08:43:11 +0100

    Show prompt to set up domain if no domain exists yet

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,20 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Enums\Role;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'Test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('test@example.com'),
+            'role' => Role::ADMIN,
+        ]);
     }
 }
